3e917a9c1c4a10c3613b4092fb0fd1fa6c5905ea
Daniel Canales <danielcanales866@gmail.com>
2026-09-01 21:52:13 -0600

    Navbar responsive

--- frontend/src/NavBar.js
+++ frontend/src/NavBar.js
@@ -4,18 +4,21 @@ import Navbar from 'react-bootstrap/Navbar';
 
 function NavBar({ setPagina }) {
   return (
-    <Navbar bg="dark" data-bs-theme="dark">
+    <Navbar bg="dark" data-bs-theme="dark" expand="lg">
       <Container>
         <Navbar.Brand onClick={() => setPagina('inicio')} style={{ cursor: 'pointer' }}>
           THE HOME OF THE DOOR
         </Navbar.Brand>
-        <Nav className="me-auto">
-          <Nav.Link onClick={() => setPagina('inicio')}>Inicio</Nav.Link>
-          <Nav.Link onClick={() => setPagina('portafolio')}>Portafolio</Nav.Link>
-          <Nav.Link onClick={() => setPagina('servicios')}>Servicios</Nav.Link>
-          <Nav.Link onClick={() => setPagina('cotizar')}>Cotizar</Nav.Link>
-          <Nav.Link onClick={() => setPagina('contacto')}>Contacto</Nav.Link>
-        </Nav>
+        <Navbar.Toggle aria-controls="menu-nav" />
+        <Navbar.Collapse id="menu-nav">
+          <Nav className="me-auto">
+            <Nav.Link onClick={() => setPagina('inicio')}>Inicio</Nav.Link>
+            <Nav.Link onClick={() => setPagina('portafolio')}>Portafolio</Nav.Link>
+            <Nav.Link onClick={() => setPagina('servicios')}>Servicios</Nav.Link>
+            <Nav.Link onClick={() => setPagina('cotizar')}>Cotizar</Nav.Link>
+            <Nav.Link onClick={() => setPagina('contacto')}>Contacto</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
       </Container>
     </Navbar>
   );
